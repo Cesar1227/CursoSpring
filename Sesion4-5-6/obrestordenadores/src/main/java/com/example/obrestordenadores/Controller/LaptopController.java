a41dd2584088846055b6e5a26ab0184ba8c16b99
Cesar1227 <bonicesar94@gmail.com>
2022-09-15 00:18:48 -0500

    Ejercicios sesión 4-5-6

---
+++ Sesion4-5-6/obrestordenadores/src/main/java/com/example/obrestordenadores/Controller/LaptopController.java
@@ -0,0 +1,4 @@
+package com.example.obrestordenadores.Controller;
+
+public class LaptopController {
+}
---
+++ Sesion4-5-6/obrestordenadores/src/test/java/com/example/obrestordenadores/ObrestordenadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obrestordenadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObrestordenadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
